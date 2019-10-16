5f1ff141a8c7376bd4cdfb7cf10563d8dbca693f
info <info@info10.cshl.edu>
2019-10-16 16:03:35 -0400

    simple operator if/else with elif statement

---
+++ PFB_problemsets/python/operators-truth-logics.1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+#using if/else and elif statements
+
+x = 0
+#x = sys.argv[1]
+
+#function = bool(x)
+
+if x > 0:
+	message = 'is greater than 0'
+	print(x. message)
+elif x < 0:
+	message = 'is less than 0'
+	print(x, message)
+else:
+	message = 'x must be 0'
+	print(x, message)
+
+
+	

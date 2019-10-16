a43ec2a782afa22bc4d2f03f220d504407f532e5
info <info@info10.cshl.edu>
2019-10-16 16:10:50 -0400

    successful if/else statement using bool() function

---
+++ PFB_problemsets/python/operators-truth-logics.2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+
+#practicing if/else using bool() function to test TRUE/FALSE of variables
+
+x = 0
+
+if bool(x) is 'TRUE':
+	print('TRUE')
+else:
+	print('NOT TRUE')
+
+
+
+	

7e577d76841c5859fa9f3159c9385ed3dfb0741e
info <info@info10.local>
2019-10-16 10:52:27 -0400

    adding favorit items to script and learning to use sys,argv

--- PFB_problemsets/python/first-py-script.py
+++ PFB_problemsets/python/first-py-script.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 myname = ('Marina')
-print(myname) 
+print('My name is:', myname)
+
+favcol = ('blue')
+print('My favorite color is:', favcol)
+
+favactivity = ('cycling long distances with hills')
+print('I enjoy', favactivity)
+
+favanimal = ('caterpillars')
+print('My favorite animals(s):', favanimal)
+
+import sys
+myname = sys.argv[1]
+favcol = sys.argv[2]
+faactivity = sys.argv[3]
+favanimal = sys.argv[4]
+print(myname, favcol, favactivity, favanimal)

14138ad3f9390752ef66a14a6558a6af7d943752
info <info@info10.cshl.edu>
2019-10-16 20:27:56 -0400

    enter number to be tested in command line, searches v.4 using sys

---
+++ PFB_problemsets/python/operators-truth-logics.4.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+
+randnum = 50
+randnum = int(sys.argv[1])
+
+if randnum < 0:
+	message = 'is negative'
+	print(randnum, message)
+elif randnum > 0:
+	message = 'is positive'
+	print(randnum, message)
+	if randnum < 50:
+		message = 'is less than 50'
+		print(randnum, message)	
+	if randnum%2 is 0:
+		message = 'is even'
+		print(randnum, message)
+	if randnum<50 and randnum%2 is 0:
+		message = 'it is an even number that is smaller than 50'
+		print(randnum, message)
+	if randnum > 50:
+		message = 'is greater than 50'
+		print(randnum, message)
+	if randnum % 3 is 0:
+		message = 'is divisible by 3'
+		print(randnum, message)
+	if randnum > 50 and randnum % 3 is 0:
+		message = 'is greater than 50 and divisible by 3'
+		print(randnum,message)
+else:
+	message = 'is zero'
+	print(randnum, message)
+
+print('random number is', randnum) 

03aec64c490707112909e45d8e38804b9d6be392
info <info@info10.cshl.edu>
2019-10-16 20:23:24 -0400

    nested loops to label positive, negative, zero, divisible by 3, evne number, or both even and >50, or >50 and divisible by 3

--- PFB_problemsets/python/operators-truth-logics.3.py
+++ PFB_problemsets/python/operators-truth-logics.3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-randnum = 40
+randnum = 50
 
 if randnum < 0:
 	message = 'is negative'
@@ -11,12 +11,21 @@ elif randnum > 0:
 	if randnum < 50:
 		message = 'is less than 50'
 		print(randnum, message)	
-		if randnum%2 is 0:
-			message = 'is even'
-			print(randnum, message)
+	if randnum%2 is 0:
+		message = 'is even'
+		print(randnum, message)
 	if randnum<50 and randnum%2 is 0:
 		message = 'it is an even number that is smaller than 50'
 		print(randnum, message)
+	if randnum > 50:
+		message = 'is greater than 50'
+		print(randnum, message)
+	if randnum % 3 is 0:
+		message = 'is divisible by 3'
+		print(randnum, message)
+	if randnum > 50 and randnum % 3 is 0:
+		message = 'is greater than 50 and divisible by 3'
+		print(randnum,message)
 else:
 	message = 'is zero'
 	print(randnum, message)
